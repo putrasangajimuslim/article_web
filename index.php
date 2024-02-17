6959840badc84668fce60aa47e4a82e98e50292b
putra sangaji muslim <putrasangajimuslim3@gmail.com>
2024-02-18 01:14:50 +0700

    updated

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="navbar">
         <span class="text-logo">BLOGSPOT</span>
 
         <div>
             <input type="text" placeholder="search.." class="search-input">
+        </div>
+    </div>
+
+    <div class="login-page">
+        <div>Login</div>
+        <div class="form-group">
+            <input type="text" placeholder="Username" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Password" class="form-control">
         </div>
     </div>
 
@@ -88,4 +100,5 @@
         </div>
     </div>
 </body>
+
 </html>
