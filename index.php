fd66759952f9782137424bcf18f44cb5c9039196
putrasangajim <Putra.Sangaji.Muslim@bni.co.id>
2024-02-17 15:41:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+        <span class="text-logo">BLOGSPOT</span>
+
+        <div>
+            <input type="text" placeholder="search.." class="search-input">
+        </div>
+    </div>
+
+    <div class="content-wrapper">
+        <div class="d-flex-space-between">
+            <div class="section-label">
+                Featured Post
+            </div>
+            <div class="content-more">
+                <span>More</span>
+            </div>
+        </div>
+        <div class="border-separator">
+            <div class="blue-box"></div>
+        </div>
+
+        <div class="container">
+            <div class="card">
+                <img src="https://via.placeholder.com/300" alt="Card Image">
+                <div class="card-content">
+                    <h2>Post Title 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                    <div class="publish-user">
+                        <img src="https://via.placeholder.com/300" alt="" style="margin-right: 10px;">
+                        <div class="user-profile">
+                            <p style="font-size:14px;">Ayunda Zikrina</p>
+                            <p style="font-size:10px;">Sept 15 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/300" alt="Card Image">
+                <div class="card-content">
+                    <h2>Post Title 2</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                    <div class="publish-user">
+                        <img src="https://via.placeholder.com/300" alt="" style="margin-right: 10px;">
+                        <div class="user-profile">
+                            <p style="font-size:14px;">Ayunda Zikrina</p>
+                            <p style="font-size:10px;">Sept 15 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/300" alt="Card Image">
+                <div class="card-content">
+                    <h2>Post Title 3</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                    <div class="publish-user">
+                        <img src="https://via.placeholder.com/300" alt="" style="margin-right: 10px;">
+                        <div class="user-profile">
+                            <p style="font-size:14px;">Ayunda Zikrina</p>
+                            <p style="font-size:10px;">Sept 15 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/300" alt="Card Image">
+                <div class="card-content">
+                    <h2>Post Title 4</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                    <div class="publish-user">
+                        <img src="https://via.placeholder.com/300" alt="" style="margin-right: 10px;">
+                        <div class="user-profile">
+                            <p style="font-size:14px;">Ayunda Zikrina</p>
+                            <p style="font-size:10px;">Sept 15 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
